ee9af5734445543650d48ac3c8a57ae1184d9051
Vergenter <okolok1998@gmail.com>
2021-06-20 15:36:19 +0200

    update video model

--- src/models/Video.py
+++ src/models/Video.py
@@ -2,51 +2,52 @@
 # pylint: disable=C0103”
 from datetime import datetime
 from py2neo.ogm import Model, Property, RelatedFrom, RelatedTo
-from models.Game import Game
-from models.Channel import Channel
+from dataclasses import asdict, dataclass
 
 
-class Video_stub(Model):
-    """Video object model"""
-    __primarylabel__ = "Video"
-    __primarykey__ = "videoId"
-    comment = RelatedFrom("Comment", "TO")
-    videoId = Property()
+@dataclass(frozen=True)
+class Channel_dc:
+    channelId: str
 
-    def __init__(self, videoId: str):
-        self.videoId = videoId
+    def __hash__(self):
+        return self.channelId
 
 
-class Video(Model):
-    """Video object model"""
-    __primarylabel__ = "Video"
-    __primarykey__ = "videoId"
-    videoId = Property()
-    etag = Property()
-    publishedAt = Property()
-    publishTime = Property()
-    title = Property()
-    description = Property()
-    liveBroadcastContent = Property()
-    game = RelatedTo(Game, "OF_GAME")
-    channel = RelatedTo(Channel, "OWNED_BY")
+@dataclass(frozen=True)
+class Video_dc:
+    videoId: str
+    etag: str
+    publishedAt: datetime
+    publishTime: datetime
+    title: str
+    description: str
+    liveBroadcastContent: bool
 
-    def __init__(self, videoId: str, etag: str, publishedAt: datetime,
-                 publishTime: datetime, title: str, description: str, liveBroadcastContent: bool):
-        self.videoId = videoId
-        self.etag = etag
-        self.publishedAt = publishedAt
-        self.publishTime = publishTime
-        self.title = title
-        self.description = description
-        self.liveBroadcastContent = liveBroadcastContent
+    def __hash__(self):
+        return self.videoId
+
+
+@dataclass(frozen=True)
+class Game_dc:
+    title: str
+    year: int
+
+    def __hash__(self):
+        return self.title
+
+
+@dataclass(frozen=True)
+class Video_row_dc:
+    video: Video_dc
+    channel: Channel_dc
+    game: Game_dc
 
 
 def fromJson(json):
     """Create Video model with relationships from json"""
-    channel = Channel(channelId=json["snippet"]["channelId"])
-    game = Game(title=json["gameTitle"], year=json["gameSubtitle"])
-    video = Video(
+    channel = Channel_dc(channelId=json["snippet"]["channelId"])
+    game = Game_dc(title=json["gameTitle"], year=json["gameSubtitle"])
+    video = Video_dc(
         videoId=json["id"]["videoId"],
         etag=json["etag"],
         publishedAt=datetime.strptime(json["snippet"]["publishedAt"],
@@ -57,7 +58,22 @@ def fromJson(json):
         description=json["snippet"]["description"],
         liveBroadcastContent=json["snippet"]["liveBroadcastContent"] != "none",
     )
-    if game.title is not None:
-        video.game.add(game)
-    video.channel.add(channel)
-    return video
+    return asdict(Video_row_dc(video, channel, game))
+
+
+video_query = '''
+UNWIND $rows AS row
+MERGE (channel:Channel{channelId: row.channel.channelId})
+MERGE (game:Game{title: row.game.title})
+CREATE (video:Video{
+    videoId: row.video.videoId,
+    etag: row.video.etag,
+    publishedAt: row.video.publishedAt,
+    publishTime: row.video.publishTime,
+    title: row.video.title,
+    description: row.video.description,
+    liveBroadcastContent: row.video.liveBroadcastContent}
+)
+CREATE (video)-[:OWNED_BY]->(channel)
+CREATE (video)-[:OF_GAME]->(game)
+'''
