068c29a17c6c23e2cf61659cc54f7e1dd0084b48
Vergenter <okolok1998@gmail.com>
2021-08-29 00:58:51 +0200

    fix update typeand and add new db

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -4,6 +4,23 @@
 CREATE SCHEMA channels
 	CREATE TABLE channels (
 		channel_id text NOT NULL,
-		update TIMESTAMP,
+		update TIMESTAMP NOT NULL,
 		CONSTRAINT channel_id_update PRIMARY KEY(channel_id,update)
 	);
+
+CREATE DATABASE videodb
+    WITH 
+    OWNER = :POSTGRES_ADMIN
+    ENCODING = 'UTF8'
+	LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+\connect videodb;
+CREATE SCHEMA videos
+	CREATE TABLE videos (
+		video_id text NOT NULL,
+		update TIMESTAMP NOT NULL,
+		CONSTRAINT video_id_update PRIMARY KEY(video_id,update)
+	)
