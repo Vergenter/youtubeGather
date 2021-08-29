85dac2907d266562fa8351ba38ca623a99d66e6c
Vergenter <okolok1998@gmail.com>
2021-08-29 06:35:10 +0200

    add include script for easy adding videos and channels

--- include/include.py
+++ include/include.py
@@ -1,12 +1,41 @@
-from utils.slice import chunked
-from include.include_config import include
-from include.include_videos_api import MAX_SINGLE_QUERY_RESULTS, fetch_videos
-import ray
+import logging
+import asyncio
+from dataclasses import asdict
+import os
+from include_config import IncludeConfig, from_yaml
+import yaml
+import aiokafka
+LOGGER_NAME = "INCLUDE"
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+log = logging.getLogger(LOGGER_NAME)
 
 
-def main(data: include):
-    # result = fetch_videos.remote(set(data.videos))
-    ids = [fetch_videos.remote(set(i)) for i in chunked(
-        MAX_SINGLE_QUERY_RESULTS, data.videos)]
-    result = ray.get(ids)
-    print(result)
+async def main(data: IncludeConfig):
+    producer = aiokafka.AIOKafkaProducer(bootstrap_servers='kafka:9092')
+    await producer.start()
+    send_channels, send_videos = 0, 0
+    try:
+        for channel in data.channels:
+            msg = str.encode(channel)
+            await producer.send_and_wait("new_channels", value=msg, key=msg)
+            send_channels += 1
+
+        for video in data.videos:
+            msg = str.encode(video)
+            await producer.send_and_wait("new_videos", value=msg, key=msg)
+            send_videos += 1
+    finally:
+        await producer.stop()
+        log.info("send %d channels and %d videos", send_channels, send_videos)
+        with open("config.yaml", 'w', encoding='utf-8') as f:
+            dumped = yaml.dump(asdict(IncludeConfig(
+                data.videos[send_channels:], data.channels[send_videos:])), Dumper=yaml.Dumper)
+            f.write(dumped)
+
+
+if __name__ == "__main__":
+    print("start script")
+    with open("config.yaml", 'r', encoding='utf-8') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+        if not not config:
+            asyncio.run(main(from_yaml(config)))
--- include/include_config.py
+++ include/include_config.py
@@ -2,6 +2,10 @@ from dataclasses import dataclass, field
 
 
 @dataclass(frozen=True)
-class include:
+class IncludeConfig:
     videos: 'list[str]' = field(default_factory=list)
     channels: 'list[str]' = field(default_factory=list)
+
+
+def from_yaml(config):
+    return IncludeConfig(videos=config.get("videos", []), channels=config.get("channels", []))
