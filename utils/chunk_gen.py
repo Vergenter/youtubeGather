712f0e34bdd123ce8de7e27092d64789e20af6ea
Vergenter <okolok1998@gmail.com>
2021-08-31 10:16:57 +0200

    updated chunking functions

--- utils/chunk_gen.py
+++ utils/chunk_gen.py
@@ -1,12 +1,16 @@
 import asyncio
+from pickle import NONE
+from typing import TypeVar, AsyncGenerator, AsyncIterator
+
+X = TypeVar('X')
 
 
-async def get_new_chunk_gen(queue: asyncio.Queue, timeout_s=0.6, max_chunk_size=2):
+async def get_new_chunk_queue(queue: asyncio.Queue[X], timeout_s: float = 0.6, max_chunk_size: int = 2) -> AsyncGenerator[list[X], None]:
     while True:
-        items = []
+        items: 'list[X]' = []
         while True:
             try:
-                item = await asyncio.wait_for(queue.get(), timeout_s)
+                item = await asyncio.wait_for(asyncio.shield(queue.get()), timeout_s)
             except asyncio.TimeoutError:
                 if len(items) > 0:
                     break
@@ -16,3 +20,26 @@ async def get_new_chunk_gen(queue: asyncio.Queue, timeout_s=0.6, max_chunk_size=
                 if len(items) >= max_chunk_size:
                     break
         yield items
+T = TypeVar('T')
+
+
+# async generator, needs python 3.6
+async def get_new_chunk_iter(it: AsyncIterator[list[T]], timeout_s: float = 0.6, max_chunk_size: int = 2) -> AsyncGenerator[list[T], None]:
+    try:
+        nxt = asyncio.ensure_future(it.__anext__())
+        while True:
+            items: 'list[T]' = []
+            while True:
+                try:
+                    item = await asyncio.wait_for(asyncio.shield(nxt), timeout_s)
+                    nxt = asyncio.ensure_future(it.__anext__())
+                except asyncio.TimeoutError:
+                    if len(items) > 0:
+                        break
+                else:
+                    items += item
+                    if len(items) >= max_chunk_size:
+                        break
+            yield items
+    except StopAsyncIteration:
+        pass
